917d390e7ffef4e9826adaf8df8d5bdd2d92091d
Babar Ali <babaralishah514@gmail.com>
2024-10-27 20:32:33 -0700

    Added incomes end points

--- Backend/index.js
+++ Backend/index.js
@@ -49,6 +49,53 @@ app.delete("/expense/:id", (req, res) => {
   res.sendStatus(200);
 });
 
+app.get("/income", (req, res) => {
+  console.log("Get all income");
+  res.send([
+    {
+      id: 1,
+      amount: 1500,
+      description: "reveived pocket money",
+      date: "2024-09-24",
+    },
+    {
+      id: 2,
+      amount: 800,
+      description: "received my selary",
+      date: "2024-09-24",
+    },
+  ]);
+});
+
+app.get("/income/:id", (req, res) => {
+  console.log("Get the income with id: " + req.params.id);
+
+  res.send({
+    id: 1,
+    amount: 1500,
+    description: "get salary",
+    date: "2024-09-24",
+  });
+});
+
+app.post("/income", (req, res) => {
+  console.log("Add income: ");
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.put("/income/:id", (req, res) => {
+  console.log("Update the income with id: " + req.params.id);
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.delete("/income/:id", (req, res) => {
+  console.log("Delete the income with id: " + req.params.id);
+  res.sendStatus(200);
+});
+
+
 app.listen(3020, function () {
   console.log("Server is running on port 3020");
 });
